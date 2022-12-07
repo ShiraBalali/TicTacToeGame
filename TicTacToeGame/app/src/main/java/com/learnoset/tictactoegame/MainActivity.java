a9d8088069fef4c314fca94eb0ec7702e5aaf227
Shira Balali <shiraba28@gmail.com>
2022-12-07 23:44:23 +0200

    added winning lines

--- TicTacToeGame/app/src/main/java/com/learnoset/tictactoegame/MainActivity.java
+++ TicTacToeGame/app/src/main/java/com/learnoset/tictactoegame/MainActivity.java
@@ -18,14 +18,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button [] buttons = new Button[9];
     private Button playAgainButton;
     private ImageView playerStatus;
+    private ImageView line;
     private int rountCount;
     boolean activePlayer;
 
     int [] gameState = {2,2,2,2,2,2,2,2,2};
 
     int [][] winningPositions = {
-            {0,1,2} , {3,4,5} , {6,7,8} , {0,3,6} , {1,4,7} , {2,5,8} , {0,4,8} , {2,4,6}
+            {0, 1, 2, R.drawable.mark6},
+            {3, 4, 5, R.drawable.mark8},
+            {6, 7, 8, R.drawable.mark7},
+            {0, 3, 6, R.drawable.mark3},
+            {1, 4, 7, R.drawable.mark4},
+            {2, 5, 8, R.drawable.mark5},
+            {0, 4, 8, R.drawable.mark1},
+            {2, 4, 6, R.drawable.mark2}
     };
+
     private Drawable x;
     private Drawable o;
     private Drawable owin;
@@ -42,7 +51,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         playerStatus = (ImageView) findViewById(R.id.playerStatus);
         playerStatus.setBackgroundResource(R.drawable.xplay);
 
+        line = (ImageView) findViewById(R.id.line_1);
+
         playAgainButton = (Button) findViewById(R.id.playAgain);
+        playAgainButton.setVisibility(View.GONE);
+        playAgainButton.setEnabled(false);
         playAgainButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -94,6 +107,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         else if(rountCount == 9){
             playerStatus.setBackgroundResource(R.drawable.nowin);
+            playAgainButton.setVisibility(View.VISIBLE);
+            playAgainButton.setEnabled(true);
         }
         else {
             activePlayer = !activePlayer;
@@ -107,8 +122,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 if(gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
                         gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
                         gameState[winningPosition[0]] != 2){
-                    winnerResult = true;
 
+                    line.setBackgroundResource(winningPosition[3]);
+                    winnerResult = true;
+                    playAgainButton.setVisibility(View.VISIBLE);
+                    playAgainButton.setEnabled(true);
                 }
             }
             return winnerResult;
@@ -116,8 +134,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         public void playAgain() {
             playerStatus.setBackgroundResource(R.drawable.xplay);
+            line.setBackgroundResource(0);
             activePlayer = true;
             rountCount = 0;
+            playAgainButton.setEnabled(false);
+            playAgainButton.setVisibility(View.GONE);
 
         for (int i=0; i < buttons.length; i++){
             gameState[i] = 2;
